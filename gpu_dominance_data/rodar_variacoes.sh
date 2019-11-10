52b4f38bf2336f557f66e75191cdcd4f64aea61c
Leonardo Santana Vieira <leosanvieira@gmail.com>
2019-11-10 12:02:14 -0200

    scripts adicionados

---
+++ gpu_dominance_data/rodar_variacoes.sh
@@ -0,0 +1,18 @@
+echo "Rodando Algoritmo com base db_1000_5000."
+./main ../bases/db_1000_5000.dat
+echo "Rodando Algoritmo com base db_2000_5000."
+./main ../bases/db_2000_5000.dat
+echo "Rodando Algoritmo com base db_3000_5000."
+./main ../bases/db_3000_5000.dat
+echo "Rodando Algoritmo com base db_4000_5000."
+./main ../bases/db_4000_5000.dat
+echo "Rodando Algoritmo com base db_5000_5000."
+./main ../bases/db_5000_5000.dat
+echo "Rodando Algoritmo com base db_5000_1000."
+./main ../bases/db_5000_1000.dat
+echo "Rodando Algoritmo com base db_5000_2000."
+./main ../bases/db_5000_2000.dat
+echo "Rodando Algoritmo com base db_5000_3000."
+./main ../bases/db_5000_3000.dat
+echo "Rodando Algoritmo com base db_5000_4000."
+./main ../bases/db_5000_4000.dat
